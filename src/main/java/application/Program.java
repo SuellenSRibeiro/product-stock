1f76f36b85e7d41709808ca7045bb066cf5ac162
Suellen Ribeiro <107809257+SuellenSRibeiro@users.noreply.github.com>
2023-01-27 15:59:44 -0300

    Update Program.java

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -20,8 +20,6 @@ public class Program {
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
 
-        System.out.println(product.name + ","+ product.price+","+product.quantity);
-
         sc.close();
     }
 }
